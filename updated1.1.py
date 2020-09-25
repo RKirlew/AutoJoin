ebcb11c30a01720e278e12b611125088dd0b361b
R_Kulio <kirlewraheem@gmail.com>
2020-09-25 16:24:37 -0500

    Create updated1.1.py
    draft pick seems to work

---
+++ updated1.1.py
@@ -0,0 +1,169 @@
+
+import pyautogui
+from time import sleep
+
+accept=None
+f= None
+r= None
+c=None
+chooseNum=0
+banNum=0
+l=None
+lockIn=None
+
+test=None
+champ= input("Which champ:")
+
+secondary=input("Which secondary:")
+secondary=secondary.lower()
+secondary=secondary.strip()
+
+champ=champ.lower()
+champ=champ.strip()
+ban= input("Which ban:")
+
+ban=ban.lower()
+ban=ban.strip()
+
+mode=input("Draft(D) or Blind (B)")
+mode=mode.lower()
+mode=mode.strip()
+
+if champ=="blitzcrank":
+    champ="blitz"
+img=champ+".png"
+secondary=secondary+".png"
+
+ban=ban+".png"
+def checkAccept():
+    global accept
+    global lockIn
+    while accept is None:
+        accept=pyautogui.locateCenterOnScreen('button.png', grayscale=False,confidence=0.5)
+        if accept:
+                     pyautogui.moveTo(accept[0],accept[1])
+                     pyautogui.click()
+                     pyautogui.moveTo(accept[0],accept[1]-150)
+                     pyautogui.click()
+                     return True
+def banChamp():
+    detectNum=0
+    b= None
+    banThis=None
+    while b is None:
+       
+
+        b=pyautogui.locateCenterOnScreen('bans.png',grayscale=False,confidence=0.5)
+        if b:
+            try:
+                print(ban)
+                test=pyautogui.locateCenterOnScreen(ban,grayscale=False,confidence=0.5)
+                pyautogui.moveTo(test[0],test[1])
+                pyautogui.click()
+                detectNum+=1
+                if banThis is None:
+                    banThis=pyautogui.locateCenterOnScreen("ban.png",grayscale=False,confidence=0.5)
+                    pyautogui.moveTo(banThis[0],banThis[1])
+                    pyautogui.click()
+            except:
+                pyautogui.scroll(-115)
+                
+def draft():
+    global c
+    global banNum
+    lockin=None
+    global img
+    print(img+" is the global variable")
+    global ban
+    global lockIn
+    global b
+    choose=None
+    b= None
+    banThis=None
+    while b is None:
+       
+
+        b=pyautogui.locateCenterOnScreen('bans.png',grayscale=False,confidence=0.5)
+        if b and banNum < 1:
+            try:
+                print(ban)
+                test=pyautogui.locateCenterOnScreen(ban,grayscale=False,confidence=0.5)
+                pyautogui.moveTo(test[0],test[1])
+                pyautogui.click()
+                banNum+=1
+               
+                if banThis is None:
+                    banThis=pyautogui.locateCenterOnScreen("ban.png",grayscale=False,confidence=0.5)
+                    pyautogui.moveTo(banThis[0],banThis[1])
+                    sleep(2)
+                    pyautogui.click()
+                    
+               
+            except:
+               
+                pyautogui.scroll(-115)
+                
+    if banThis is not None:
+        print("banning phase over")
+        while choose is None:
+                choose=pyautogui.locateCenterOnScreen("choose.png",grayscale=False,confidence=0.7)
+        if choose:
+    
+                    #pyautogui.moveTo(choose[0],choose[1])
+                    print("Choose banner detected!")
+                    while c is None:
+                        try:
+                            print("C is none")
+                            c=pyautogui.locateCenterOnScreen(img,grayscale=False,confidence=0.5)
+                            print(c)
+                            print(c[0])
+                            print(c[1])
+                            pyautogui.moveTo(c[0],c[1])
+                            pyautogui.click()
+                            c=1
+                            if lockin is None:
+                                lockin=pyautogui.locateCenterOnScreen("lock-in.png",grayscale=False,confidence=0.5)
+                                pyautogui.moveTo(lockin[0],lockin[1])
+                                sleep(2)
+                                pyautogui.click()
+                                lockIn=1
+                                chooseNum+=1
+                                if chooseNum>1:
+                                    img=secondary
+                        except:
+                            pyautogui.scroll(-115)
+
+
+               
+                    
+if mode == 'd':
+    #draft()
+    #checkAccept()
+   while lockIn is None:
+        #checkAccept()
+        draft()
+
+if mode =='b':
+            while c is None:
+                while l is None:
+                    l=pyautogui.locateCenterOnScreen("choose.png",grayscale=False,confidence=0.5)
+                if l:
+                    try:
+                        print(img+" was chosen")
+                        c=pyautogui.locateCenterOnScreen(img,grayscale=False,confidence=0.5)
+                        pyautogui.moveTo(c[0],c[1])
+                        pyautogui.click()
+                        c=1
+                        if lockin is None:
+                            lockin=pyautogui.locateCenterOnScreen("lock-in.png",grayscale=False,confidence=0.5)
+                            pyautogui.moveTo(lockin[0],lockin[1])
+                            pyautogui.click()
+                    except:
+                        pyautogui.scroll(-115)
+                   
+        
+    
+   
+        
+       
+print(c)
